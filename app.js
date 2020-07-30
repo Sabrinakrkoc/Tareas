0888e49d06644c1cff10ba0535a9a40e119a8b8e
sabrina <krkoc86@gmail.com>
2020-07-30 13:33:06 -0300

    app tareas

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 
 const path = require('path');
-//incluir los modulos de npm q vamos a usar
 const express = require('express');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index')
 const app = express();
 
-//conectame a la base de datos
 mongoose.connect('mongodb+srv://sakrkoc:k32224139@cluster0.tjbcv.mongodb.net/cursosit', {useNewUrlParser:true})
  .then(db => console.log('base conectada'))
  .catch(err => console.log('Ocurrio un error', err))
 
 
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 7000)
 
 
 app.set('views', path.join(__dirname, 'views'));
